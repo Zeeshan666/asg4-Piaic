5cf141633d0e38e20cd7598118e0af6ffee6fe69
Zeeshan  Manzoor <zeeshanmanzoor.zm@gmail.com>
2019-11-03 01:04:13 -0700

    2nd comit after changing dates

--- task12.js
+++ task12.js
@@ -1,7 +1,8 @@
-var d = new Date();
-var d2 = new Date(1979,2,4);
- var ml = d2.getMilliseconds()
+var now=new Date();
+console.log('Current date :'+now);
+var a=Date.now();
+console.log('Elapsed Miliseconds since January 1, 1970 :'+a);
+var minutes=((a/1000) / 60) / 60;
+console.log('Elapsed minutes since January 1,1970 :'+minutes);
 
 
-console.log("current date:"+d)
-console.log("elapsed milliseconds:"+ml)
---
+++ task15.js
@@ -0,0 +1,6 @@
+var now=new Date();
+console.log('Current date :'+now);
+
+var d = new Date();
+d.setHours(9);
+console.log('1 hour ago it was '+d);
